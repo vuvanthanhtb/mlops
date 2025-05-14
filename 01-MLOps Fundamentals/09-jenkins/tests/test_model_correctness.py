7b815764f73c1653e426f9c451084655b8b02154
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-05-14 21:59:48 +0700

    day 9

--- 01-MLOps Fundamentals/09-jenkins/tests/test_model_correctness.py
+++ 01-MLOps Fundamentals/09-jenkins/tests/test_model_correctness.py
@@ -5,18 +5,18 @@ import pandas as pd
 MODEL_DIR = "models"
 
 def test_model_correctness():
-    clf = joblib.load(f"{MODEL_DIR}/model.pkl")
-    data = {
-        "MSSubClass": 60,
-        "MSZoning": "RL",
-        "LotArea": 7844,
-        "LotConfig": "Inside",
-        "BldgType": "1Fam",
-        "OverallCond": 7,
-        "YearBuilt": 1978,
-        "YearRemodAdd": 1978,
-        "Exterior1st": "HdBoard",
-        "BsmtFinSF2": 0,
-        "TotalBsmtSF": 672
-    }
-    assert clf.predict(pd.DataFrame(data, index=[0]))[0] == 157551.3761237591
+  clf = joblib.load(f"{MODEL_DIR}/model.pkl")
+  data = {
+    "MSSubClass": 60,
+    "MSZoning": "RL",
+    "LotArea": 7844,
+    "LotConfig": "Inside",
+    "BldgType": "1Fam",
+    "OverallCond": 7,
+    "YearBuilt": 1978,
+    "YearRemodAdd": 1978,
+    "Exterior1st": "HdBoard",
+    "BsmtFinSF2": 0,
+    "TotalBsmtSF": 672
+  }
+  assert clf.predict(pd.DataFrame(data, index=[0]))[0] == 157551.3761237591
